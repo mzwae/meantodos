086e21814f3c2a80e2c4163d1b82ab7a6d8bb718
mzwae <mrmonjaie@gmail.com>
2017-10-23 07:10:28 +0100

    Added REST API

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
-var password = require('../password');//<username>:<password>
-var db = mongojs('mongodb://' + password.value  + '@ds127375.mlab.com:27375/meantodos', ['todos']);
+var password = require('../password'); //<username>:<password>
+var db = mongojs('mongodb://' + password.value + '@ds127375.mlab.com:27375/meantodos', ['todos']);
 
 // Get Todos
 router.get('/todos', function (req, res, next) {
@@ -16,7 +16,7 @@ router.get('/todos', function (req, res, next) {
 });
 
 
-//Get Todo
+// Get Single Todo
 router.get('/todo/:id', function (req, res, next) {
   db.todos.findOne({
     _id: mongojs.ObjectId(req.params.id)
@@ -28,5 +28,82 @@ router.get('/todo/:id', function (req, res, next) {
     }
   });
 });
+
+
+
+
+// Save Todo
+router.post('/todo', function (req, res, next) {
+  var todo = req.body;
+  if (!todo.text || !(todo.isCompleted + '')) {
+    res.status(400);
+    res.json({
+      "error": "Invalid Data"
+    });
+  } else {
+    db.todos.save(todo, function (err, result) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json(result);
+      }
+    });
+  }
+});
+
+
+// Update Todo
+router.put('/todo/:id', function (req, res, next) {
+  var todo = req.body;
+  var updObj = {};
+
+  if (todo.isCompleted) {
+    updObj.isCompleted = todo.isCompleted;
+  }
+
+  if (todo.text) {
+    updObj.text = todo.text;
+  }
+
+  if (!updObj) {
+    res.status(400);
+    res.json({
+      "error": "Invalid Data"
+    });
+  } else {
+    db.todos.update({
+      _id: mongojs.ObjectId(req.params.id)
+    }, updObj, {}, function (err, result) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json(result);
+      }
+    });
+  }
+});
+
+
+// Delete Todo
+router.delete('/todo/:id', function (req, res, next) {
+  db.todos.remove({
+    _id: mongojs.ObjectId(req.params.id)
+  }, '', function (err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(result);
+    }
+
+  });
+});
+
+
+
+
+
+
+
+
 
 module.exports = router;
