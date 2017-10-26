c25ef3cff2115e5b577797b8cf13466aa28ca34a
mzwae <mrmonjaie@gmail.com>
2017-10-26 09:55:54 +0100

    heroku maintain

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').load();
 var express = require('express');
 var path = require('path');
 var bodyparser = require('body-parser');
@@ -16,6 +17,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'client')));
 app.set('views', path.join(__dirname, 'client', 'index.html'));
 app.use('/api/v1/', todos);
+
+module.exports = app;
 
 //app.listen(3000, function(){
 //  console.log('Server started on port 3000...');
